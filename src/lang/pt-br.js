01f944ab0336cd8b2bfc7e85e8c712414ee093d0
Nathã <natha.web@hotmail.com>
2017-10-28 17:13:29 -0200

    add: translated messages in brazilian portuguese

---
+++ src/lang/pt-br.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang("pbckcode", "pt-br",
+  {
+    title: 'PBCKCODE',
+    addCode: 'Adicionar Código',
+    editCode: 'Editar Código',
+    editor: 'Editor',
+    settings: 'Configurações',
+    mode: 'Modo',
+    tabSize: 'Espaçamento do Tab',
+    theme: 'Tema',
+    softTab: 'Habilitar abas suaves',
+    emmet: 'Habilitar Emmet'
+  });
