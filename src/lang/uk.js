196993883d770e5ebc58a5c543e39417d140798a
Mykola Pukhalskyi <notfound.la2.asterios.tm@gmail.com>
2018-11-05 12:23:49 +0200

    Create uk.js (#52)
    Added Ukrainian translation

---
+++ src/lang/uk.js
@@ -0,0 +1,13 @@
+CKEDITOR.plugins.setLang("pbckcode", "uk",
+  {
+    title: 'PBCKCODE',
+    addCode: 'Додати код',
+    editCode: 'Змінити код',
+    editor: 'Редактор',
+    settings: 'Налаштування',
+    mode: 'Режим',
+    tabSize: 'Ширина табуляції',
+    theme: 'Тема',
+    softTab: 'Увімкнути м’яку табуляцію',
+    emmet: 'Увімкнути Emmet'
+  });
